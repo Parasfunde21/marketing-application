cda3122c769fab22bddc5f4d21aa4c42bc130033
paras <parasfunde9@gmail.com>
2026-03-10 12:19:27 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./components"),
+      "@hooks": path.resolve(__dirname, "./hooks"),
+      "@lib": path.resolve(__dirname, "./lib"),
+      "@pages": path.resolve(__dirname, "./pages")
+    }
+  }
 });
